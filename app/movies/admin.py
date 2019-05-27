b163af4b0251bf86c3c89232ed39fd45c3d613fd
Norbiox <norberto9411@gmail.com>
2019-05-27 11:57:28 +0200

    models created

--- app/movies/admin.py
+++ app/movies/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Actor,
+    Comment,
+    Country,
+    Director,
+    Genre,
+    Producer,
+    RatingSource,
+    Writer,
+    Movie,
+    MovieRating,
+    MovieWriter
+)
+
+
+admin.site.register(Actor)
+admin.site.register(Comment)
+admin.site.register(Country)
+admin.site.register(Director)
+admin.site.register(Genre)
+admin.site.register(Producer)
+admin.site.register(RatingSource)
+admin.site.register(Writer)
+admin.site.register(Movie)
+admin.site.register(MovieRating)
+admin.site.register(MovieWriter)
--- app/movies/models.py
+++ app/movies/models.py
@@ -1,3 +1,104 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class Actor(m.Model):
+    name = m.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(m.Model):
+    text = m.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+
+
+class Country(m.Model):
+    name = m.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(m.Model):
+    name = m.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(m.Model):
+    name = m.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(m.Model):
+    name = m.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Producer(m.Model):
+    name = m.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class RatingSource(m.Model):
+    name = m.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Writer(m.Model):
+    name = m.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(m.Model):
+    actors = m.ManyToManyField(Actor)
+    awards = m.CharField(max_length=100)
+    box_office = m.IntegerField()
+    comments = m.ManyToManyField(Comment)
+    countries = m.ManyToManyField(Country)
+    directors = m.ManyToManyField(Director)
+    dvd = m.DateField()
+    genres = m.ManyToManyField(Genre)
+    imdb_rating = m.FloatField()
+    imdb_id = m.CharField(max_length=20)
+    imdb_votes = m.IntegerField()
+    languages = m.ManyToManyField(Language)
+    metascore = m.IntegerField()
+    plot = m.CharField(max_length=500)
+    poster = m.URLField(max_length=200)
+    production = m.ForeignKey('Producer', related_name='movies',
+                              on_delete=m.CASCADE)
+    rated = m.CharField(max_length=5)
+    released = m.DateField()
+    response = m.BooleanField()
+    runtime = m.CharField(max_length=10)
+    title = m.CharField(max_length=200)
+    type = m.CharField(max_length=20)
+    website = m.URLField(max_length=200)
+    year = m.IntegerField()
+
+
+class MovieRating(m.Model):
+    movie = m.ForeignKey('Movie', related_name='ratings', on_delete=m.CASCADE)
+    source = m.ForeignKey('RatingSource', related_name='movies', on_delete=m.CASCADE)
+    value = m.CharField(max_length=10)
+
+
+class MovieWriter(m.Model):
+    movie = m.ForeignKey('Movie', related_name='writers', on_delete=m.CASCADE)
+    writer = m.ForeignKey('Writer', related_name='movies', on_delete=m.CASCADE)
+    notes = m.CharField(max_length=200)
