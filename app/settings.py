b164addb4d4be013ff08b6a9c4c66a65c5e1207e
Norbiox <norberto9411@gmail.com>
2019-05-29 08:49:32 +0200

    database connection implemented

--- app/settings.py
+++ app/settings.py
@@ -1,18 +1,20 @@
 import os
 import django_heroku
+import dj_database_url
+import dotenv
 
-
-django_heroku.settings(locals())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 OMDBAPIKEY = os.environ.get('OMDBAPIKEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG_VALUE', ''))
 
 ALLOWED_HOSTS = [
@@ -35,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -63,12 +66,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -97,3 +96,12 @@ USE_TZ = True
 
 DATE_FORMAT = "%d %b %Y"
 ISO_DATE_FORMAT = "%Y-%m-%d"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
